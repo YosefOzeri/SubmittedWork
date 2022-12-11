59de7a84f1401e4abd6f8f516b6609b7ba6adaff
Aviya Yechezkel <117904424+Aviyay318@users.noreply.github.com>
2022-12-11 14:37:03 +0200

    Update Check5.java

--- src/Check5.java
+++ src/Check5.java
@@ -1,6 +1,6 @@
 public class Check5 {
     public static void main(String[] args) {
-        System.out.println("ram?");
+        System.out.println("omg it's workkkkkkkkkkk");
         System.out.println("yosef :)");
         System.out.println("aviya");
     }
