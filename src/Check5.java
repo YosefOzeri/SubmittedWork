448344ca4357e158b6541cb75f88e77f23b7b2e1
Yosef Ozeri <ozeriyosef1@gmail.com>
2022-12-11 14:20:22 +0200

    smile

--- src/Check5.java
+++ src/Check5.java
@@ -1,6 +1,6 @@
 public class Check5 {
     public static void main(String[] args) {
-        system.out.println("ram?");
+        System.out.println("ram?");
         System.out.println("yosef :)");
         System.out.println("aviya");
     }
